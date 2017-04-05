675002de08c52ce1f92638aa30d5f67f86b7430e
MagicKitty <nicolasblin585@gmail.com>
2017-04-05 12:04:34 +0200

    oui

--- tinyrender/main.cpp
+++ tinyrender/main.cpp
@@ -5,6 +5,7 @@
 #include "geometry.h"
 #include <iostream>
 #include <stdlib.h>
+#include <limits>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -13,7 +14,7 @@ Model *model = NULL;
 const int width  = 2000;
 const int height = 2000;
 
-void rasterize(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
+void triangleZBuffer(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color);
 
 void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -38,44 +39,69 @@ void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color) {
     }
 }
 
-void triangle2(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
-    rasterize(t0,t1,t2,image,color);
-}
-
-void rasterize(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-    int minx = (int)std::min(t0.x,std::min(t1.x,t2.x));
-    int maxx = (int)std::max(t0.x,std::max(t1.x,t2.x));
-    int miny = (int)std::min(t0.y,std::min(t1.y,t2.y));
-    int maxy = (int)std::max(t0.y,std::max(t1.y,t2.y));
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
+    if (t0.y==t1.y && t0.y==t2.y) return; //degenerated triangles
+    int minx = std::min(t0.x,std::min(t1.x,t2.x));
+    int maxx = std::max(t0.x,std::max(t1.x,t2.x));
+    int miny = std::min(t0.y,std::min(t1.y,t2.y));
+    int maxy = std::max(t0.y,std::max(t1.y,t2.y));
+    int dx10=t1.x-t0.x;
+    int dx02=t0.x-t2.x;
+    int dx21=t2.x-t1.x;
+    int dy10=t1.y-t0.y;
+    int dy02=t0.y-t2.y;
+    int dy21=t2.y-t1.y;
     for(int j=miny;j<maxy;j++){
+        int dyj0=j-t0.y;
+        int dyj2=j-t2.y;
+        int dyj1=j-t1.y;
         for(int i=minx;i<maxx;i++){
-            if( ((t1.x-t0.x)*(j-t0.y)-(t1.y-t0.y)*(i-t0.x)<=0)&&
-                ((t0.x-t2.x)*(j-t2.y)-(t0.y-t2.y)*(i-t2.x)<=0)&&
-                ((t2.x-t1.x)*(j-t1.y)-(t2.y-t1.y)*(i-t1.x)<=0)){
+            if(((dx10*dyj0-dy10*(i-t0.x)>=0)&&
+                (dx02*dyj2-dy02*(i-t2.x)>=0)&&
+                (dx21*dyj1-dy21*(i-t1.x)>=0))||
+               ((dx10*dyj0-dy10*(i-t0.x)<=0)&&
+                (dx02*dyj2-dy02*(i-t2.x)<=0)&&
+                (dx21*dyj1-dy21*(i-t1.x)<=0))){
                 image.set(i, j, color);
+            }
         }
     }
-}
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
-    if (t0.y>t1.y) std::swap(t0, t1);
-    if (t0.y>t2.y) std::swap(t0, t2);
-    if (t1.y>t2.y) std::swap(t1, t2);
-    int total_height = t2.y-t0.y;
-    for (int i=0; i<total_height; i++) {
-        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
-        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
-        float alpha = (float)i/total_height;
-        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
-        Vec2i A =               t0 + (t2-t0)*alpha;
-        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
-        if (A.x>B.x) std::swap(A, B);
-        for (int j=A.x; j<=B.x; j++) {
-            image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y
+void triangleZBuffer(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+    Vec3f t0 = pts[0];
+    Vec3f t1 = pts[1];
+    Vec3f t2 = pts[2];
+    if (t0.y==t1.y && t0.y==t2.y) return; //degenerated triangles
+    int minx = std::min(t0.x,std::min(t1.x,t2.x));
+    int maxx = std::max(t0.x,std::max(t1.x,t2.x));
+    int miny = std::min(t0.y,std::min(t1.y,t2.y));
+    int maxy = std::max(t0.y,std::max(t1.y,t2.y));
+    int dx10=t1.x-t0.x;
+    int dx02=t0.x-t2.x;
+    int dx21=t2.x-t1.x;
+    int dy10=t1.y-t0.y;
+    int dy02=t0.y-t2.y;
+    int dy21=t2.y-t1.y;
+    for(int j=miny;j<maxy;j++){
+        int dyj0=j-t0.y;
+        int dyj2=j-t2.y;
+        int dyj1=j-t1.y;
+        for(int i=minx;i<maxx;i++){
+            if(((dx10*dyj0-dy10*(i-t0.x)>=0)&&
+                (dx02*dyj2-dy02*(i-t2.x)>=0)&&
+                (dx21*dyj1-dy21*(i-t1.x)>=0))||
+               ((dx10*dyj0-dy10*(i-t0.x)<=0)&&
+                (dx02*dyj2-dy02*(i-t2.x)<=0)&&
+                (dx21*dyj1-dy21*(i-t1.x)<=0))){
+                image.set(i, j, color);
+            }
         }
     }
+}
+
+Vec3f world2screen(Vec3f v) {
+    return Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
 }
 
 int main(int argc, char** argv) {
@@ -86,21 +112,30 @@ int main(int argc, char** argv) {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
+
     Vec3f light_dir(0,0,-1);
+
+    float *zbuffer = new float[width*height];
+    for (int i=0;i<width*height; i++) {
+        zbuffer[i] = -std::numeric_limits<float>::max();
+    }
+
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
+        Vec3f pts_s[3];
+        Vec3f pts_w[3];
         Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j=0; j<3; j++) {
+        for (int j=0; j<3; j++){
             Vec3f v = model->vert(face[j]);
             screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
-            world_coords[j]  = v;
+            pts_s[j] = world2screen(model->vert(face[j]));
+            pts_w[j] = model->vert(face[j]);
         }
-        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        Vec3f n = (pts_w[2]-pts_w[0])^(pts_w[1]-pts_w[0]);
         n.normalize();
         float intensity = n*light_dir;
         if (intensity>0) {
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+            triangleZBuffer(pts_s, zbuffer, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
         }
     }
 
